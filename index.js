c4bbca5d91438e735c33ee9cbce1fb7dd3361828
Samuel Witke <witkesam@gmail.com>
2018-01-20 19:19:09 -0500

    Cooking With fire

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const Player = require('player');
 let app = express();
-let player = new Player('./Guy Fieri Saying Flavortown.mp3');
+let player = new Player('./SONG MADE FROM GUY FIERI SOUNDS (FLAVORTOWN REMIX).mp3');
 
 app.get('/', (req, res) => {
-	try {
-		player.play();
-	} catch(err) {
-		console.log(err);
-	}
+    player.play(function(err, player){
+      console.log('playend!');
+    });
 	res.send('Ferri server started');
 });
 
